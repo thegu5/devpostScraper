346e212b94fb50b40d0c03455229f54b10e394dc
thegu5 <58223632+thegu5@users.noreply.github.com>
2023-02-22 19:03:13 -0500

    get project info for every project every participant has made

--- scrape.py
+++ scrape.py
@@ -16,8 +16,24 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 # Find the last page link
 pagination = soup.find("ul", {"class": "pagination"})
-last_page_link = pagination.find_all("a")[-2]
-last_page = int(last_page_link.text)
+last_page = 1
+if pagination:
+    last_page_link = pagination.find_all("a")[-2]
+    last_page = int(last_page_link.text)
+
+
+def parse_divs(webpage):
+    projs = []
+    divs = webpage.find_all("div", {"class": "software-entry gallery-entry fade visible"})
+    for div in divs:
+        proj = {}
+        proj["name"] = div.find("h5").text.strip()
+        proj["url"] = div.parent["href"]
+        creators = div.find_all("img", class_="user-photo")
+        proj["creators"] = [creator["title"] for creator in creators]
+        projs.append(proj)
+    return projs
+
 
 # Scrape projects on each page
 projects = []
@@ -26,20 +42,33 @@ for page in tqdm(range(1, last_page + 1), desc="Scraping project pages", unit="p
     url = f"{hackathon_url}?page={page}"
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     soup = BeautifulSoup(response.text, "html.parser")
-    project_divs = soup.find_all("div", {"class": "software-entry gallery-entry fade visible"})
+    project_divs = soup.find_all("a", {"class": "block-wrapper-link fade link-to-software"})
     for project_div in project_divs:
-        project_name = project_div.find("h5").text.strip()
-        creators = project_div.find_all("img", class_="user-photo")
-        creators_names = [creator["title"] for creator in creators]
-        users += creators_names
-        project_url = project_div.parent["href"]
-        projects.append({"name": project_name, "creators": creators_names, "url": project_url})
+        projects.append(project_div["href"])
+        user_cards = project_div.find_all("img", class_="user-photo")
+        users += [user_card["title"] for user_card in user_cards]
+# At this point, projects will be an array of urls and users will be an array of usernames
 
-# Scrape hackathons that each individual project submitted to
-for project in tqdm(projects, desc="Scraping project details", unit="projects"):
-    url = project["url"]
+for user in tqdm(users, desc="Scraping user details", unit="users"):
+    if user == "":
+        continue
+    url = f"https://devpost.com/{user}"
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     soup = BeautifulSoup(response.text, "html.parser")
+    project_divs = soup.find_all("a", {"class": "block-wrapper-link fade link-to-software"})
+    for project_div in project_divs:
+        projects.append(project_div["href"])
+    # replace the user's name with the user object
+    # TODO: take into account user page pagination
+print(projects)
+# Scrape hackathons that each individual project submitted to
+data = []
+for project in tqdm(projects, desc="Scraping project details", unit="projects"):
+    url = project
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    soup = BeautifulSoup(response.text, "html.parser")
+    project = {}
+    project["url"] = url
     submissions = soup.find("div", {"id": "submissions"})
     if submissions:
         hackathons = submissions.find_all("a")
@@ -50,6 +79,7 @@ for project in tqdm(projects, desc="Scraping project details", unit="projects"):
     if links:
         links = links.find_all("a")
         project["links"] = [link["href"] for link in links]
+    # THIS MIGHT BE BROKEN IDK
     project["video"] = ""
     video = soup.find("a", {"class": "ytp-title-link yt-uix-sessionlink"})
     if video:
@@ -58,28 +88,13 @@ for project in tqdm(projects, desc="Scraping project details", unit="projects"):
     if container:
         container = container.find_all("div", {"class": None})
         project["description"] = container[-1].get_text()
+    data.append(project)
 
-for project in projects:
-    project["hackathons"] = list(filter(None, project["hackathons"]))
+# remove empty hackathon strings
+for project in data:
+    if "hackathons" in project:
+        project["hackathons"] = [hackathon for hackathon in project["hackathons"] if hackathon]
 
-for user in tqdm(users, desc="Scraping user details", unit="users"):
-    if user == "":
-        continue
-    url = f"https://devpost.com/{user}"
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    soup = BeautifulSoup(response.text, "html.parser")
-    user_projects = soup.find_all("a", {"class": "block-wrapper-link fade link-to-software"})
-    user = {"name": user, "url": url, "projects": []}
-    for project in user_projects:
-        user["projects"].append({
-            "name": project.find("h5").text.strip(),
-            "url": project["href"]
-        })
-    # replace the user's name with the user object
-    users[users.index(user["name"])] = user
-    # TODO: take into account user page pagination
-
-data = {"projects": projects, "users": users}
 timenow = strftime("%Y-%m-%d-%H:%M:%S", gmtime())
 with open(f"{hackathon_name}-{timenow}.json", "w") as f:
     json.dump(data, f, indent=4)
